3a987ec739f78e0e981e0d31c7f3a8da691a442d
heyjinjung <heyjinjung@users.noreply.github.com>
2026-02-04 12:39:46 +0900

    fix: down_revision을 올바른 마이그레이션으로 수정

--- alembic/versions/20260203_0100_add_baseline_charge_amount.py
+++ alembic/versions/20260203_0100_add_baseline_charge_amount.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '20260203_0100_add_baseline_charge_amount'
-down_revision = '20260203_1000_add_v2_external_deposit_unmatched'
+down_revision = '20260202_1530_extend_v2_user_segment'
 branch_labels = None
 depends_on = None
 
